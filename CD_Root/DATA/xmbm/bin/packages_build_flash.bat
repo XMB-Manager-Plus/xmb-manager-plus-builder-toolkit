734d85f05add92df29bec7caeb192aa1a58bd9d7
andreus-sebes <andreus.sebes@gmail.com>
2012-05-13 20:44:10 +0100

    resolved bug with rco when creating flash pkg

--- CD_Root/DATA/xmbm/bin/packages_build_flash.bat
+++ CD_Root/DATA/xmbm/bin/packages_build_flash.bat
@@ -11,12 +11,18 @@ call "%bindir%\global_messages.bat" "BUILDING"
 if exist "%pkgsource%\flash" rmdir /Q /S "%pkgsource%\flash"
 if not exist "%pkgsource%\flash" mkdir "%pkgsource%\flash"
 xcopy /E "%pkgbaseflash%\*.*" "%pkgsource%\flash" >NUL
+FOR /F "tokens=*" %%A IN ('CD') DO FOR %%B IN (%%~A) DO SET CurDir=%%B
+
 for /f "tokens=1,2 delims=*" %%X IN ('dir /b %pkgsource%\flash\XMBMPFLSH\USRDIR\resource\*.355') DO (
-%external%\rcomage\Rcomage\rcomage.exe compile "%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X\%%X.xml" "%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X.rco"
+cd "%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X"
+"%CurDir%\%external%\rcomage\Rcomage\rcomage.exe" compile "%CurDir%\%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X\%%X.xml" "%CurDir%\%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X.rco"
+cd %CurDir%
 rmdir /Q /S "%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X
 )
 for /f "tokens=1,2 delims=*" %%X IN ('dir /b %pkgsource%\flash\XMBMPFLSH\USRDIR\resource\*.341') DO (
-%external%\rcomage\Rcomage\rcomage.exe compile "%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X\%%X.xml" "%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X.rco"
+cd "%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X"
+"%CurDir%\%external%\rcomage\Rcomage\rcomage.exe" compile "%CurDir%\%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X\%%X.xml" "%CurDir%\%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X.rco"
+cd %CurDir%
 rmdir /Q /S "%pkgsource%\flash\XMBMPFLSH\USRDIR\resource\%%X
 )
 %external%\%packager% package-flash.conf %pkgsource%\flash\XMBMPFLSH
