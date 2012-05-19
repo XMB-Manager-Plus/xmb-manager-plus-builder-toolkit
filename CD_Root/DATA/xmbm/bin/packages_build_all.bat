bc0cc713d70ad6af6b7076846ea35c4e8a3d693d
andreus-sebes <andreus.sebes@gmail.com>
2012-05-19 16:19:42 +0100

    corrected bugs

--- CD_Root/DATA/xmbm/bin/packages_build_all.bat
+++ CD_Root/DATA/xmbm/bin/packages_build_all.bat
@@ -5,20 +5,30 @@ call "%bindir%\global_prechecks.bat" %0
 
 :first
 if not exist %pkgsource%\core-hdd0\%id_xmbmp% goto :error_source
+if not exist %pkgsource%\flash\%id_xmbmp_flash% goto :error_source
+if not exist %pkgsource%\themepacks goto :error_source
+if not exist %pkgsource%\languagepacks goto :error_source
 call "%bindir%\global_messages.bat" "BUILDING"
 for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.355"') DO (
 cd "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
 "%~dp0\%external%\rcomage\Rcomage\rcomage.exe" compile "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X\%%X.xml" "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X.rco"
 cd "%~dp0"
-::rmdir /Q /S "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X
+move "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X" "%pkgsource%\flash\"
 )
 for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.341"') DO (
 cd "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
 "%~dp0\%external%\rcomage\Rcomage\rcomage.exe" compile "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X\%%X.xml" "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X.rco"
 cd "%~dp0"
-::rmdir /Q /S "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
+move "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X" "%pkgsource%\flash\"
 )
 %external%\%packager% %pkgsource%\package-flash.conf %pkgsource%\flash\%id_xmbmp_flash%
+for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\*.355"') DO (
+move "%pkgsource%\flash\%%X" "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\"
+)
+for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\*.341"') DO (
+move "%pkgsource%\flash\%%X" "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\"
+)
+del /Q /S "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.rco"
 rename UP0001-%id_xmbmp_flash%_00-0000000000000000.pkg XMB_Manager_Plus_v%working_version%_Flash.pkg
 %external%\%packager% %pkgsource%\package-xmbmp.conf %pkgsource%\core-hdd0\%id_xmbmp%
 rename UP0001-%id_xmbmp%_00-0000000000000000.pkg XMB_Manager_Plus_v%working_version%_Core.pkg
--- CD_Root/DATA/xmbm/bin/packages_build_flash.bat
+++ CD_Root/DATA/xmbm/bin/packages_build_flash.bat
@@ -12,18 +12,26 @@ for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\%id_xmbmp_flash%
 cd "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
 "%~dp0\%external%\rcomage\Rcomage\rcomage.exe" compile "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X\%%X.xml" "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X.rco"
 cd "%~dp0"
-::rmdir /Q /S "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X
+move "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X" "%pkgsource%\flash\"
 )
 for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.341"') DO (
 cd "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
 "%~dp0\%external%\rcomage\Rcomage\rcomage.exe" compile "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X\%%X.xml" "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X.rco"
 cd "%~dp0"
-::rmdir /Q /S "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
+move "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X" "%pkgsource%\flash\"
 )
+
 %external%\%packager% %pkgsource%\package-flash.conf %pkgsource%\flash\%id_xmbmp_flash%
+for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\*.355"') DO (
+move "%pkgsource%\flash\%%X" "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\"
+)
+for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\*.341"') DO (
+move "%pkgsource%\flash\%%X" "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\"
+)
+del /Q /S "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.rco"
 rename UP0001-%id_xmbmp_flash%_00-0000000000000000.pkg XMB_Manager_Plus_v%working_version%_Flash.pkg
 if not exist "%pkgoutput%" mkdir "%pkgoutput%"
-move %bindir%\*.pkg "%pkgoutput%\"
+move "%bindir%\*.pkg" "%pkgoutput%\"
 call "%bindir%\global_messages.bat" "BUILD-OK"
 goto :end
 
