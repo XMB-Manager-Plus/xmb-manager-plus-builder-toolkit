f220287a93c8bec5515396f51aa57e319e78797e
andreus-sebes <andreus.sebes@gmail.com>
2012-05-19 20:58:19 +0100

    forgot to remove rems

--- CD_Root/DATA/xmbm/bin/packages_build_all.bat
+++ CD_Root/DATA/xmbm/bin/packages_build_all.bat
@@ -9,27 +9,27 @@ if not exist %pkgsource%\flash\%id_xmbmp_flash% goto :error_source
 if not exist %pkgsource%\themepacks goto :error_source
 if not exist %pkgsource%\languagepacks goto :error_source
 call "%bindir%\global_messages.bat" "BUILDING"
-REM for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.355"') DO (
-REM cd "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
-REM "%~dp0\%external%\rcomage\Rcomage\rcomage.exe" compile "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X\%%X.xml" "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X.rco"
-REM cd "%~dp0"
-REM move "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X" "%pkgsource%\flash\"
-REM )
-REM for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.341"') DO (
-REM cd "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
-REM "%~dp0\%external%\rcomage\Rcomage\rcomage.exe" compile "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X\%%X.xml" "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X.rco"
-REM cd "%~dp0"
-REM move "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X" "%pkgsource%\flash\"
-REM )
-REM %external%\%packager% %pkgsource%\package-%id_xmbmp_flash%.conf %pkgsource%\flash\%id_xmbmp_flash%
-REM for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\*.355"') DO (
-REM move "%pkgsource%\flash\%%X" "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\"
-REM )
-REM for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\*.341"') DO (
-REM move "%pkgsource%\flash\%%X" "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\"
-REM )
-REM del /Q /S "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.rco"
-REM rename UP0001-%id_xmbmp_flash%_00-0000000000000000.pkg XMB_Manager_Plus_v%working_version%_Flash.pkg
+for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.355"') DO (
+cd "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
+"%~dp0\%external%\rcomage\Rcomage\rcomage.exe" compile "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X\%%X.xml" "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X.rco"
+cd "%~dp0"
+move "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X" "%pkgsource%\flash\"
+)
+for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.341"') DO (
+cd "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X"
+"%~dp0\%external%\rcomage\Rcomage\rcomage.exe" compile "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X\%%X.xml" "%~dp0\%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X.rco"
+cd "%~dp0"
+move "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\%%X" "%pkgsource%\flash\"
+)
+%external%\%packager% %pkgsource%\package-%id_xmbmp_flash%.conf %pkgsource%\flash\%id_xmbmp_flash%
+for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\*.355"') DO (
+move "%pkgsource%\flash\%%X" "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\"
+)
+for /f "tokens=1,2 delims=*" %%X IN ('dir /b "%pkgsource%\flash\*.341"') DO (
+move "%pkgsource%\flash\%%X" "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\"
+)
+del /Q /S "%pkgsource%\flash\%id_xmbmp_flash%\USRDIR\resource\*.rco"
+rename UP0001-%id_xmbmp_flash%_00-0000000000000000.pkg XMB_Manager_Plus_v%working_version%_Flash.pkg
 %external%\%packager% %pkgsource%\package-%id_xmbmp%.conf %pkgsource%\core-hdd0\%id_xmbmp%
 rename UP0001-%id_xmbmp%_00-0000000000000000.pkg XMB_Manager_Plus_v%working_version%_Core.pkg
 pause
